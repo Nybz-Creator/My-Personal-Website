5787eeca0416cd59af34ebaf8be58f0a885ee8d3
Alexander Grönlund <143708577+Nybz-Creator@users.noreply.github.com>
2024-03-25 15:42:33 +0100

    Add files via upload

--- My personal website/java.js
+++ My personal website/java.js
@@ -1,8 +1,10 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+/// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
 
 // Get the navbar
-var navbar = document.getElementById("vertical-menu");
+var navbar = document.getElementsByClassName(".vertical-menu");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -10,28 +12,26 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+      navbar.classList.add("sticky");
   } else {
-    navbar.classList.remove("sticky");
+      navbar.classList.remove("sticky");
   }
 }
+
 
 // Get the button
-let mybutton = document.getElementById("myBtn");
+let scrollTopBtn = document.getElementById("scrollTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+      scrollTopBtn.style.display = "block"; // Make the button visible
   } else {
-    mybutton.style.display = "none";
+      scrollTopBtn.style.display = "none"; // Hide the button
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// When the user clicks on the button, scroll to the top of the document smoothly
+scrollTopBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
